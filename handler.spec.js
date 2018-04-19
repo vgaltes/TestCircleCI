006f3b25a63a41d751e2ed3e0fb8b8c0c57b7f22
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-04-19 23:04:08 +0100

    First step

--- handler.spec.js
+++ handler.spec.js
@@ -1,7 +1,14 @@
 const mocha = require('mocha');
+const chai = require('chai');
+const should = chai.should();
 
-describe("A simple test", () => {
-    it("passes", () => {
+const handler = require('./handler');
 
+describe("The handler function", () => {
+    it("returns a message", () => {
+        handler.hello(undefined, undefined, function(error, response){
+            let body = JSON.parse(response.body);
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+        });
     });
 });
