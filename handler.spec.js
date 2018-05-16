f32b0d18cc4e3c860a3f5383024f3230589ba6a1
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-05-16 22:42:34 +0100

    Added environment variables

--- handler.spec.js
+++ handler.spec.js
@@ -6,9 +6,10 @@ const handler = require('./handler');
 
 describe("The handler function", () => {
     it("returns a message", () => {
+        process.env.MESSAGE = "message"
         handler.hello(undefined, undefined, function(error, response){
             let body = JSON.parse(response.body);
-            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+            body.message.should.be.equal(`The message is: message`);
         });
     });
 });
